27fa48c5aedc7503083a98e01c161fa885387a14
dsahdr <72198703+dsahdr@users.noreply.github.com>
2020-11-24 11:27:40 +0300

    path

--- scanner/services/last_block_persister.py
+++ scanner/services/last_block_persister.py
@@ -12,12 +12,12 @@ class LastBlockPersister:
 
     def get_last_block(self) -> int:
         try:
-            with open(os.path.join('../', self.base_dir, self.network_name), 'r') as file:
+            with open(os.path.join(self.base_dir, self.network_name), 'r') as file:
                 last_block_number = file.read()
         except FileNotFoundError:
             return 1
         return int(last_block_number)
 
     def save_last_block(self, last_block_number: int):
-        with open(os.path.join('../', self.base_dir, self.network_name), 'w') as file:
+        with open(os.path.join(self.base_dir, self.network_name), 'w') as file:
             file.write(str(last_block_number))
