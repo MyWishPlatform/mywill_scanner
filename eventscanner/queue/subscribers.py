d4c8441a08307e0079ece9924fc96be6a99f4b9b
dsahdr <72198703+dsahdr@users.noreply.github.com>
2020-11-24 16:12:39 +0300

    Update subscribers.py

--- eventscanner/queue/subscribers.py
+++ eventscanner/queue/subscribers.py
@@ -6,3 +6,7 @@ from eventscanner.monitors.contract.deploy import DeployMonitor
 pub.subscribe(MatPaymentMonitor.on_new_block_event, 'MATIC_MAINNET')
 pub.subscribe(ERC20PaymentMonitor.on_new_block_event, 'MATIC_MAINNET')
 pub.subscribe(DeployMonitor.on_new_block_event, 'MATIC_MAINNET')
+
+pub.subscribe(MatPaymentMonitor.on_new_block_event, 'MATIC_TESTNET')
+pub.subscribe(ERC20PaymentMonitor.on_new_block_event, 'MATIC_TESTNET')
+pub.subscribe(DeployMonitor.on_new_block_event, 'MATIC_TESTNET')
