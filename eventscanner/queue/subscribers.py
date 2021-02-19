53fb5320afeb7735b7a3d8bd8c7c8a145391e365
Ameize4 <ameize4@gmail.com>
2021-02-19 21:50:14 +0300

    Add base monitor class

--- eventscanner/queue/subscribers.py
+++ eventscanner/queue/subscribers.py
@@ -4,10 +4,10 @@ from settings import CONFIG
 from .. import monitors
 
 for name, monitor_config in CONFIG["monitors"].items():
-    monitor = getattr(monitors, name, None)
-    if monitor:
-        method = monitor_config.get("method", None)
+    monitor_class = getattr(monitors, name, None)
+    if monitor_class:
         networks = monitor_config["networks"]
 
         for network in networks:
+            monitor = monitor_class(network)
             pub.subscribe(monitor.on_new_block_event, network)
