2ad1e6a5e5dd97853aa98c9afdffb290896fad20
Ameize4 <ameize4@gmail.com>
2021-02-24 16:38:07 +0300

    Fix problem with pubsub lib

--- eventscanner/queue/subscribers.py
+++ eventscanner/queue/subscribers.py
@@ -3,6 +3,7 @@ from pubsub import pub
 from settings import CONFIG
 from .. import monitors
 
+subscribe_list = []
 for name, monitor_config in CONFIG["monitors"].items():
     monitor_class = getattr(monitors, name, None)
     if monitor_class:
@@ -10,4 +11,9 @@ for name, monitor_config in CONFIG["monitors"].items():
 
         for network in networks:
             monitor = monitor_class(network)
-            pub.subscribe(monitor.on_new_block_event, network)
+            subscribe_list.append((monitor.on_new_block_event, network))
+
+# pubsub lib do not remember variables, created inside loop.
+# So, we create list of variables in one loop, and then push it to pubsub
+for elem in subscribe_list:
+    pub.subscribe(*elem)
