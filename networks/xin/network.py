e1c90f28156c83cc964ec0c1e3b821afa8dd5623
maximepifanov <epifanov.maxim2014@yandex.ru>
2021-05-28 14:55:34 +0300

    add XinFin integration

--- networks/xin/network.py
+++ networks/xin/network.py
@@ -11,8 +11,7 @@ from settings import CONFIG
 
 class XinNetwork(Network):
     def __init__(self, type):
-        super().__init_()
-
+        super().__init_(type)
         xinscan_api_key = CONFIG['networks'][type].get('xinscan_api_key')
         is_testnet = CONFIG['networks'][type].get('is_testnet')
         xinscan = XinFinScanAPI(xinscan_api_key, is_testnet) if xinscan_api_key else None
