8c51e570b9b54dc824acbffa3dfad1b56b448db0
maximepifanov <epifanov.maxim2014@yandex.ru>
2021-05-28 15:49:09 +0300

    add XinFin integration

--- networks/xin/starter.py
+++ networks/xin/starter.py
@@ -6,7 +6,7 @@ from networks.xin.scanner import XinScanner
 class XinMaker:
 
     def __init__(self, network_name: str, polling_interval: int, commitment_chain_length: int):
-        network = XinNetwork(network_name)
+        network = XinNetwork()
         last_block_persister = LastBlockPersister(network)
         self.scanner = XinScanner(network, last_block_persister,
                                   polling_interval, commitment_chain_length)
