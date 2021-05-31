3aa01561732576f23fc8d6c47fffc4d70a18a6b7
maximepifanov <epifanov.maxim2014@yandex.ru>
2021-06-01 01:02:20 +0300

    add XinFin integration

--- networks/xin/network.py
+++ networks/xin/network.py
@@ -1,7 +1,6 @@
 import ast
 import http.client
 
-
 from hexbytes import HexBytes
 
 from base import Block, Output, Transaction, TransactionReceipt, Network
@@ -9,45 +8,42 @@ from networks.xin.xin_api import XinFinScanAPI
 from settings import CONFIG
 
 
-def get_last_block():
-    conn = http.client.HTTPSConnection("rpc.xinfin.network")
-
-    payload = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_blockNumber\",\"params\":[],\"id\":83}"
-
-    headers = {'content-type': "application/json"}
-
-    conn.request("POST", "//blockNumber", payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
-
-    return int.from_bytes(data, byteorder='big', signed=True)
-
-
-def get_tx_receipt():
-    conn = http.client.HTTPSConnection("rpc.xinfin.network")
-
-    payload = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getTransactionReceipt\",\"params\":[" \
-              "\"0xa3ece39ae137617669c6933b7578b94e705e765683f260fcfe30eaa41932610f\"],\"id\":1} "
-
-    headers = {'content-type': "application/json"}
-
-    conn.request("POST", "//getTransactionReceipt", payload, headers)
-
-    res = conn.getresponse()
-    tx_res = res.read()
-    tx_dict = tx_res.decode("UTF-8")
-    tx_data = ast.literal_eval(tx_dict)
-
-    return TransactionReceipt(
-        tx_data['transactionHash'].hex(),
-        tx_data['contractAddress'],
-        tx_data['logs'],
-        bool(tx_data['status']),
-    )
-
-
 class XinNetwork(Network):
+    def get_last_block(self):
+        conn = http.client.HTTPSConnection("rpc.xinfin.network")
+
+        payload = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_blockNumber\",\"params\":[],\"id\":83}"
+
+        headers = {'content-type': "application/json"}
+
+        conn.request("POST", "//blockNumber", payload, headers)
+        res = conn.getresponse()
+        data = res.read()
+        print(data.decode("utf-8"))
+
+        return int.from_bytes(data, byteorder='big', signed=True)
+
+    def get_tx_receipt(self, hash):
+        conn = http.client.HTTPSConnection("rpc.xinfin.network")
+
+        payload = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getTransactionReceipt\",\"params\":[" \
+                  "\"0xa3ece39ae137617669c6933b7578b94e705e765683f260fcfe30eaa41932610f\"],\"id\":1} "
+
+        headers = {'content-type': "application/json"}
+
+        conn.request("POST", "//getTransactionReceipt", payload, headers)
+
+        res = conn.getresponse()
+        tx_res = res.read()
+        tx_dict = tx_res.decode("UTF-8")
+        tx_data = ast.literal_eval(tx_dict)
+
+        return TransactionReceipt(
+            tx_data['transactionHash'].hex(),
+            tx_data['contractAddress'],
+            tx_data['logs'],
+            bool(tx_data['status']),
+        )
 
     def __init__(self, type):
         print('1Xin ' + type)
@@ -58,15 +54,15 @@ class XinNetwork(Network):
         is_testnet = CONFIG['networks'][type].get('is_testnet')
         xinscan = XinFinScanAPI(xinscan_api_key, is_testnet) if xinscan_api_key else None
 
-    #
-    # self.erc20_contracts_dict = {t_name: self.rpc.eth.contract(
-    #     self.rpc.toChecksumAddress(t_address),
-    #     abi=erc20_abi
-    # ) for t_name, t_address in CONFIG['erc20_tokens'].items()}
+        #
+        # self.erc20_contracts_dict = {t_name: self.rpc.eth.contract(
+        #     self.rpc.toChecksumAddress(t_address),
+        #     abi=erc20_abi
+        # ) for t_name, t_address in CONFIG['erc20_tokens'].items()}
 
     def get_block(self, number: int) -> Block:
         conn = http.client.HTTPSConnection("rpc.xinfin.network")
-        payload = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[true, latest],\"id\":}"
+        payload = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[true, latest],\"id\":1}"
 
         headers = {'content-type': "application/json"}
 
@@ -119,7 +115,7 @@ class XinNetwork(Network):
         )
         return t
 
-    # def get_processed_tx_receipt(self, tx_hash, token_name):
-    #     tx_data = get_tx_receipt(tx_hash)
-    #     processed = self.erc20_contracts_dict[token_name].events.Transfer().processReceipt(tx_data)
-    # return processed
+# def get_processed_tx_receipt(self, tx_hash, token_name):
+#     tx_data = get_tx_receipt(tx_hash)
+#     processed = self.erc20_contracts_dict[token_name].events.Transfer().processReceipt(tx_data)
+# return processed
