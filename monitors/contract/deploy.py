92fa6573799b2c5ae84ed86cb3c3bb89b0fb5a43
maximepifanov <epifanov.maxim2014@yandex.ru>
2021-06-01 20:11:24 +0300

    add XinFin integration

--- monitors/contract/deploy.py
+++ monitors/contract/deploy.py
@@ -10,7 +10,8 @@ class DeployMonitor(BaseMonitor):
         print('евент блок 1')
         deploy_hashes = {}
         for transactions_list in block_event.transactions_by_address.values():
-            print(transactions_list)
+            transactions_list_dic = transactions_list.__dict__
+            print(transactions_list_dic)
             print("лист транзакций")
             print(block_event)
             print("блок эвент 2")
