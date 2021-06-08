d8c6ab7e8f55ddcb64c0e1c820443d372a8ffad0
maximepifanov <epifanov.maxim2014@yandex.ru>
2021-06-08 14:15:40 +0300

    add XinFin integration

--- networks/xin/scanner.py
+++ networks/xin/scanner.py
@@ -3,18 +3,4 @@ from networks.eth.scanner import EthScanner
 
 
 class XinScanner(EthScanner):
-
-    def process_block(self, block: Block):
-        pass
-
-    def _check_tx_from(self, tx, addresses):
-        pass
-
-    def _check_tx_to(self, tx, address):
-        pass
-
-    def _check_tx_creates(self, tx, address):
-        pass
-
-    def _find_event(self, block: Block) -> dict:
-        pass
+    pass
