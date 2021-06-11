1a2dcc3e147b2a28539c72923ddaf4296ae713dd
maximepifanov <epifanov.maxim2014@yandex.ru>
2021-06-11 19:15:41 +0300

    add XinFin integration

--- monitors/payments/eth_payment_monitor.py
+++ monitors/payments/eth_payment_monitor.py
@@ -17,7 +17,7 @@ class EthPaymentMonitor(BaseMonitor):
     def on_new_block_event(self, block_event: BlockEvent):
         print('ABOBUS')
         addresses = block_event.transactions_by_address.keys()
-        print(block_event)
+        print(block_event.__dict__)
         print('АДРЕСА')
         print(addresses)
         user_site_balances = session.query(UserSiteBalance).filter(UserSiteBalance.eth_address.in_(addresses)).all()
