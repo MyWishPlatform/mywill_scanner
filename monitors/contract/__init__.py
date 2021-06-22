70d994b924adcdebb771c418508ffe0c5af39453
Pechenuca <maxim.epifanov8@gmail.com>
2021-06-22 12:37:55 +0300

    first try

--- monitors/contract/__init__.py
+++ monitors/contract/__init__.py
@@ -2,3 +2,4 @@ from .airdrop import AirdropMonitor
 from .deploy import DeployMonitor
 from .initialized import InitializedMonitor
 from .ownership_transferred import OwnershipMonitor
+from .finalized import FinalizedMonitor
