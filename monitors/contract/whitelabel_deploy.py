a7713afd29a49a20fe3a950601d47bcfa2c5a590
Kirill Bergman <k.bergman@protonmail.com>
2021-07-09 20:33:35 +0300

    test multiple launches fix

--- monitors/contract/whitelabel_deploy.py
+++ monitors/contract/whitelabel_deploy.py
@@ -14,12 +14,10 @@ class EthSendingMonitor(BaseMonitor):
 
         whitelabels = []
         for detail in tokens_details:
-            result = session.query(Contract, Network, detail)\
-                .filter(Contract.id == detail.contract_id)\
-                .filter(Contract.network_id == Network.id)\
-                .filter(detail.white_label_hash.in_(deploy_hashes.keys()))\
-                .filter(Network.name == block_event.network.type).all()
-            whitelabels.extend(result)
+            result = session.query(detail).options.joinedload(Contract, Network).all()
+            filtered = result.filter(detail.white_label_hash.in_(deploy_hashes.keys()))\
+                .filter(Contract.id == detail.contract_id & Contract.network.name == block_event.network.type)
+            whitelabels.extend(filtered)
 
         for catched_detail in whitelabels:
             print("contract_id: ", catched_detail.contract_id, 'white_label hash: ', catched_detail.white_label_hash)
