cfdc332673d532aae0f1129277b950d831264f37
Kirill Bergman <k.bergman@protonmail.com>
2021-07-09 20:36:19 +0300

    test multiple launches fix

--- monitors/contract/whitelabel_deploy.py
+++ monitors/contract/whitelabel_deploy.py
@@ -1,5 +1,6 @@
 from base import BlockEvent, BaseMonitor, Transaction
 from models import session, tokens_details, Contract, Network
+from sqlalchemy.orm import joinedload
 
 
 class EthSendingMonitor(BaseMonitor):
@@ -14,7 +15,7 @@ class EthSendingMonitor(BaseMonitor):
 
         whitelabels = []
         for detail in tokens_details:
-            result = session.query(detail).options.joinedload(Contract, Network).all()
+            result = session.query(detail).options(joinedload(Contract, Network)).all()
             filtered = result.filter(detail.white_label_hash.in_(deploy_hashes.keys()))\
                 .filter(Contract.id == detail.contract_id & Contract.network.name == block_event.network.type)
             whitelabels.extend(filtered)
