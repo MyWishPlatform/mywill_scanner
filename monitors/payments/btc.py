927f942d07ec4ab50ab3f5679ce64b62ca378216
reloverevolt <savitskiy.revenway@gmail.com>
2021-12-24 17:37:55 +0300

    [ADDED] sentFrom

--- monitors/payments/btc.py
+++ monitors/payments/btc.py
@@ -1,3 +1,6 @@
+import json
+import requests
+
 from base import BlockEvent, BaseMonitor
 from models import UserSiteBalance, session
 from settings import CONFIG
@@ -15,17 +18,32 @@ class BTCPaymentMonitor(BaseMonitor):
             raise TypeError(f'currency field should be specified for {self.network_type} network.')
         self.currency = currency
 
+    def get_sent_from_address(self, tx_hash):
+        ntype = 'testnet'
+        if self.network_type.split('_')[1] == 'MAINNET':
+            ntype = 'mainnet'
 
-    def address_from(self, model):
+        tx_info_url = f'https://api.bitcore.io/api/{self.currency}/{ntype}/tx/{tx_hash}/coins'
+        res = requests.get(tx_info_url)
+        if not res.ok:
+            return None
+        tx_info = res.json()
+        inputs = tx_info.get('inputs', None)
+        first_input = inputs[0] if inputs and len(inputs) != 0 else None
+        if not first_input or not first_input.get('address', None):
+            return None
+        return first_input['address']
+
+    def get_sent_to_address(self, model):
         s = self.currency.lower() + '_address'
         return getattr(model, s)
-    
 
     def on_new_block_event(self, block_event: BlockEvent):
         addresses = block_event.transactions_by_address.keys()
-        user_site_balances = session.query(UserSiteBalance).filter(self.address_from(UserSiteBalance).in_(addresses)).all()
+        user_site_balances = session.query(UserSiteBalance).filter(
+            self.get_sent_to_address(UserSiteBalance).in_(addresses)).all()
         for user_site_balance in user_site_balances:
-            address = self.address_from(user_site_balance)
+            address = self.get_sent_to_address(user_site_balance)
             transactions = block_event.transactions_by_address[address]
             if not transactions:
                 print('{}: User {} received from DB, but was not found in transaction list (block {}).'.format(
@@ -37,9 +55,9 @@ class BTCPaymentMonitor(BaseMonitor):
                         print('{}: Found transaction out from internal address. Skip it.'
                               .format(block_event.network.type), flush=True)
                         continue
-
                     message = {
                         'exchangeId': user_site_balance.id,
+                        'fromAddress': self.get_sent_from_address(transaction.tx_hash),
                         'address': address,
                         'transactionHash': transaction.tx_hash,
                         'currency': self.currency,
@@ -47,6 +65,5 @@ class BTCPaymentMonitor(BaseMonitor):
                         'success': True,
                         'status': 'COMMITTED'
                     }
-
                 self.send_to_backend(message)
 
